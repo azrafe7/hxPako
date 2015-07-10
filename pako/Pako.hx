ad6bbbb89fce6641d3901c704ca18e9a38f30962
azrafe7 <azrafe7@gmail.com>
2015-07-10 22:23:29 +0200

    Pako.hx (also adds a version field)

---
+++ pako/Pako.hx
@@ -0,0 +1,176 @@
+package pako;
+
+import haxe.io.UInt8Array;
+import haxe.zip.FlushMode;
+import pako.Deflate.DeflateOptions;
+import pako.Inflate.InflateOptions;
+import pako.zlib.Deflate as ZlibDeflate;
+import pako.utils.Common;
+import pako.zlib.Messages;
+import pako.zlib.ZStream;
+import pako.zlib.Constants;
+import pako.zlib.Constants.CompressionLevel;
+import pako.zlib.GZHeader;
+
+
+class Pako
+{
+  /** Version of ported pako lib (https://github.com/nodeca/pako). */
+  inline static public var VERSION:String = "0.2.7";
+  
+  /**
+   * deflate(data[, options]) -> Uint8Array|Array|String
+   * - data (Uint8Array|Array|String): input data to compress.
+   * - options (Object): zlib deflate options.
+   *
+   * Compress `data` with deflate alrorythm and `options`.
+   *
+   * Supported options are:
+   *
+   * - level
+   * - windowBits
+   * - memLevel
+   * - strategy
+   *
+   * [http://zlib.net/manual.html#Advanced](http://zlib.net/manual.html#Advanced)
+   * for more information on these.
+   *
+   * Sugar (options):
+   *
+   * - `raw` (Boolean) - say that we work with raw stream, if you don't wish to specify
+   *   negative windowBits implicitly.
+   * - `to` (String) - if equal to 'string', then result will be "binary string"
+   *    (each char code [0..255])
+   *
+   * ##### Example:
+   *
+   * ```javascript
+   * var pako = require('pako')
+   *   , data = Uint8Array([1,2,3,4,5,6,7,8,9]);
+   *
+   * console.log(pako.deflate(data));
+   * ```
+   **/
+  static public function deflate(input:UInt8Array, ?options:DeflateOptions) {
+    var deflator = new Deflate(options);
+
+    //NOTE(hx): change here if we accept mode as Bool too
+    deflator.push(input, Flush.Z_FINISH);
+
+    // That will never happens, if you don't cheat with options :)
+    if (deflator.err != 0) { throw deflator.msg; }
+
+    return deflator.result;
+  }
+
+
+  /**
+   * deflateRaw(data[, options]) -> Uint8Array|Array|String
+   * - data (Uint8Array|Array|String): input data to compress.
+   * - options (Object): zlib deflate options.
+   *
+   * The same as [[deflate]], but creates raw data, without wrapper
+   * (header and adler32 crc).
+   **/
+  static public function deflateRaw(input:UInt8Array, ?options:DeflateOptions) {
+    if (options == null) options = { };
+    options.raw = true;
+    return deflate(input, options);
+  }
+
+
+  /**
+   * gzip(data[, options]) -> Uint8Array|Array|String
+   * - data (Uint8Array|Array|String): input data to compress.
+   * - options (Object): zlib deflate options.
+   *
+   * The same as [[deflate]], but create gzip wrapper instead of
+   * deflate one.
+   **/
+  static public function gzip(input:UInt8Array, ?options:DeflateOptions) {
+    if (options == null) options = { };
+    options.gzip = true;
+    return deflate(input, options);
+  }
+  /**
+   * inflate(data[, options]) -> Uint8Array|Array|String
+   * - data (Uint8Array|Array|String): input data to decompress.
+   * - options (Object): zlib inflate options.
+   *
+   * Decompress `data` with inflate/ungzip and `options`. Autodetect
+   * format via wrapper header by default. That's why we don't provide
+   * separate `ungzip` method.
+   *
+   * Supported options are:
+   *
+   * - windowBits
+   *
+   * [http://zlib.net/manual.html#Advanced](http://zlib.net/manual.html#Advanced)
+   * for more information.
+   *
+   * Sugar (options):
+   *
+   * - `raw` (Boolean) - say that we work with raw stream, if you don't wish to specify
+   *   negative windowBits implicitly.
+   * - `to` (String) - if equal to 'string', then result will be converted
+   *   from utf8 to utf16 (javascript) string. When string output requested,
+   *   chunk length can differ from `chunkSize`, depending on content.
+   *
+   *
+   * ##### Example:
+   *
+   * ```javascript
+   * var pako = require('pako')
+   *   , input = pako.deflate([1,2,3,4,5,6,7,8,9])
+   *   , output;
+   *
+   * try {
+   *   output = pako.inflate(input);
+   * } catch (err)
+   *   console.log(err);
+   * }
+   * ```
+   **/
+  static public function inflate(input:UInt8Array, ?options:InflateOptions) {
+    var inflator = new Inflate(options);
+
+    inflator.push(input, true);
+
+    // That will never happens, if you don't cheat with options :)
+    if (inflator.err != ErrorStatus.Z_OK) { throw inflator.msg; }
+
+    return inflator.result;
+  }
+
+
+  /**
+   * inflateRaw(data[, options]) -> Uint8Array|Array|String
+   * - data (Uint8Array|Array|String): input data to decompress.
+   * - options (Object): zlib inflate options.
+   *
+   * The same as [[inflate]], but creates raw data, without wrapper
+   * (header and adler32 crc).
+   **/
+  static public function inflateRaw(input:UInt8Array, ?options:InflateOptions) {
+    if (options == null) options = { };
+    options.raw = true;
+    return inflate(input, options);
+  }
+
+
+  /**
+   * ungzip(data[, options]) -> Uint8Array|Array|String
+   * - data (Uint8Array|Array|String): input data to decompress.
+   * - options (Object): zlib inflate options.
+   *
+   * Just shortcut to [[inflate]], because it autodetects format
+   * by header.content. Done for convenience.
+   **/
+}
+
+/*
+exports.Deflate = Deflate;
+exports.deflate = deflate;
+exports.deflateRaw = deflateRaw;
+exports.gzip = gzip;
+*/
